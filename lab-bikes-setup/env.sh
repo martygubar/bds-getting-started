53641681746d5a250d62922f53b76ba40c91f329
martygubar <martin.gubar@oracle.com>
2020-10-19 15:58:09 -0400

    updated defaults

--- lab-bikes-setup/env.sh
+++ lab-bikes-setup/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Setup environment for data used for workshop
 
-export COMPARTMENT_OCID="ocid1.compartment.oc1..aaaaaaaaflmh4xzhvwti3o7jwarpveik57tjr4p6g6whyppibkhcc5qo7s2a"
-export BUCKET_NAME="cloudsqlworkshop"
+export COMPARTMENT_OCID=""
+export BUCKET_NAME="livelabs"
 export HDFS_ROOT="/data"
 export TARGET_DIR="$HOME/Downloads"
