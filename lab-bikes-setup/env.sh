3de5dc4f69fe0ff43a60fdd1c47d35385ff035e3
martygubar <martin.gubar@oracle.com>
2020-10-19 15:59:50 -0400

    update to env

--- lab-bikes-setup/env.sh
+++ lab-bikes-setup/env.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Setup environment for data used for workshop
 
+# Object Store:  ocid for the compartment.
 export COMPARTMENT_OCID=""
+
+# Object Store:  Bucket where data will be copied
 export BUCKET_NAME="livelabs"
+
+# HDFS:  root directory in HDFS for the data
 export HDFS_ROOT="/data"
+
+# HDFS:  local target directory where files will be downloaded
 export TARGET_DIR="$HOME/Downloads"
