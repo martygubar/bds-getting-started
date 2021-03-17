a17adfe4197a95562464c7ec7cd4ee4aeee0b6dd
martygubar <martin.gubar@oracle.com>
2021-03-17 17:42:45 -0400

    not showing compartment name

--- lab-bikes-setup/download-all-objstore.sh
+++ lab-bikes-setup/download-all-objstore.sh
@@ -4,8 +4,8 @@
 SCRIPT_FULL_PATH=$(dirname "$0")
 . $SCRIPT_FULL_PATH/env.sh
 
-compartment_name=`oci iam compartment get --compartment-id $COMPARTMENT_OCID | jq '.data.name'`
-echo "Downloading to compartment: $compartment_name"
+#compartment_name=`oci iam compartment get --compartment-id $COMPARTMENT_OCID | jq '.data.name'`
+#echo "Downloading to compartment: $compartment_name"
 echo ""
 echo "Getting scripts."
 wget $SOURCE_PREFIX/download-citibikes-objstore.sh
@@ -18,7 +18,7 @@ echo "Running scripts."
 
 
 echo "Done."
-compartment_name=`oci iam compartment get --compartment-id $COMPARTMENT_OCID | jq -r '.data.name'`
+#compartment_name=`oci iam compartment get --compartment-id $COMPARTMENT_OCID | jq -r '.data.name'`
 echo "Data can be found in:"
-echo "   Compartment:  $compartment_name"
+# echo "   Compartment:  $compartment_name"
 echo "        Bucket:  $BUCKET_NAME"
